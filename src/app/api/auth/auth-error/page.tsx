d7dabaf8083f329487df7965dbdf38d3b641e5fe
Your New Name <vipinkumarti555@gmail.com>
2025-07-13 20:30:11 +0530

    auth success and error page

--- src/app/api/auth/auth-error/page.tsx
+++ src/app/api/auth/auth-error/page.tsx
@@ -1,12 +1,33 @@
 "use client";
+import Link from "next/link";
 import React from "react";
+import { RxCross2 } from "react-icons/rx";
 
 const page = () => {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center">
-      <h1 className="">
-       <span> Click here to go back to </span><a href="/api/auth/sign-in">Sign In</a> 
-      </h1>
+    <div className="flex h-[50vh] w-full items-center justify-center">
+      <div className="flex h-fit max-w-xl flex-col items-center justify-center gap-4 rounded-lg bg-zinc-300 p-5 text-xl text-nowrap shadow-md transition-colors">
+        <div className="group relative flex items-center justify-center">
+          <svg width="120" height="120" className="circle">
+            <circle cx="60" cy="60" r="50" stroke="#82181a" />
+          </svg>
+          <RxCross2 className="tick absolute h-20 w-20 text-red-900 opacity-100 transition-all duration-1000 ease-out" />
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h1 className="text-3xl">Server Issue </h1>
+          <p className="flex flex-col items-center justify-center gap-4 text-lg">
+            {" "}
+           Something went wrong on our end. Please try again later.
+          </p>
+          <Link
+            href={"/api/auth/sing-in"}
+            className="flex w-fit items-center gap-4 rounded-md bg-blue-500 px-4 py-2 text-zinc-200 outline-0 transition-all duration-150 ease-out hover:bg-blue-600"
+          >
+            Sign In
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/api/auth/verify-request/page.tsx
+++ src/app/api/auth/verify-request/page.tsx
@@ -1,20 +1,33 @@
-//TODO:compelete the route and the profile page too 
+//TODO:compelete the route and the profile page too
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { TiTick } from "react-icons/ti";
 
 const page = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center text-xl sm:text-3xl">
-      <h1 className="flex items-center justify-center flex-col gap-4 shadow-md  p-5 rounded-lg transition-colors h-96 ">
-        {" "}
-        click here Check the Authentication Mail{" "}
-        <a
-          href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
-          className="flex items-center gap-4 rounded-md bg-blue-500 px-4 py-2 text-zinc-200 outline-0 transition-all duration-150 ease-out hover:bg-blue-600"
-        >
-          <FcGoogle /> Mail
-        </a>{" "}
-      </h1>
+    <div className="flex h-[50vh] w-full items-center justify-center ">
+      <div className="flex h-fit max-w-xl flex-col items-center justify-center gap-4 rounded-lg p-5 text-xl text-nowrap shadow-md transition-colors bg-zinc-300 ">
+        <div className="group relative flex items-center justify-center">
+          <svg width="120" height="120" className="circle">
+            <circle cx="60" cy="60" r="50"  className="stroke-[#008000]" />
+          </svg>
+          <TiTick className="tick absolute h-20 w-20 text-[#008000] opacity-100 transition-all duration-1000 ease-out" />
+        </div>
+
+        <div className="flex items-center justify-center flex-col gap-2">
+          <h1 className="text-3xl">Mail Sent Successfully</h1>
+          <p className="flex flex-col items-center justify-center gap-4 text-sm">
+            {" "}
+            Please check your inbox to verify your email.
+          </p>
+            <a
+              href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+              className="flex items-center gap-4 rounded-md bg-blue-500 px-4 py-2 text-zinc-200 outline-0 transition-all duration-150 ease-out hover:bg-blue-600 w-fit"
+            >
+              <FcGoogle /> Mail
+            </a>{" "}
+        </div>
+      </div>
     </div>
   );
 };
