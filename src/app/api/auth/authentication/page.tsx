88b45d67c6616d68824c299429e8de23952b369f
Your New Name <vipinkumarti555@gmail.com>
2025-10-10 15:04:39 +0530

    ss

--- src/app/api/auth/authentication/page.tsx
+++ src/app/api/auth/authentication/page.tsx
@@ -3,7 +3,7 @@ import SignIn from "./SignIn";
 import { checkIsAuthenticated } from "~/server/auth/checkIsAuthenticated";
 import { redirect } from "next/navigation";
 
-const page: React.FC = async () => {
+const Page: React.FC = async () => {
   const isAuthenticated = await checkIsAuthenticated();
   if (isAuthenticated) {
     redirect("/");
